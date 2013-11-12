180fccf7f1a92ef0ee27bbf9057be09e7fd8deb8
gronowski <terence.gronowski@gmail.com>
2013-11-12 20:56:29 +0100

    Erstes Thread Beispiel

---
+++ MeinThread.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: teri
+ * Date: 12.11.13
+ * Time: 20:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class MeinThread implements Runnable{
+    public void run(){
+        for (int i=0;i<10;i++)
+            System.out.println(i);
+    }
+
+    public static void main(String[] args){
+        Thread t0=new Thread(new MeinThread());
+        t0.start();
+    }
+}
