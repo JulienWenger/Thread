7ae124cd0202dc6e1e081c25a477d9dd76c08067
gronowski <terence.gronowski@gmail.com>
2013-11-12 21:37:02 +0100

    Dritter Commit, benannter Thread

--- MeinThread.java
+++ MeinThread.java
@@ -8,13 +8,15 @@
 public class MeinThread implements Runnable{
     public synchronized void run(){
         for (int i=0;i<10;i++)
-            System.out.println(i);
+            System.out.println(Thread.currentThread().getName()+" "+ i);
     }
 
     public static void main(String[] args){
         Thread t0=new Thread(new MeinThread());
+        t0.setName("T1");
         t0.start();
         Thread t1=new Thread(new MeinThread());
+        t1.setName("T2");
         t1.start();
     }
 }
