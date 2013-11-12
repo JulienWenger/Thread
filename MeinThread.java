54d250b127549410004b2f8f9c757d7fa50c6ecc
gronowski <terence.gronowski@gmail.com>
2013-11-12 21:24:37 +0100

    Zweiter Commit

--- MeinThread.java
+++ MeinThread.java
@@ -6,7 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 public class MeinThread implements Runnable{
-    public void run(){
+    public synchronized void run(){
         for (int i=0;i<10;i++)
             System.out.println(i);
     }
@@ -14,5 +14,7 @@ public class MeinThread implements Runnable{
     public static void main(String[] args){
         Thread t0=new Thread(new MeinThread());
         t0.start();
+        Thread t1=new Thread(new MeinThread());
+        t1.start();
     }
 }
