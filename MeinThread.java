e211b92a156e846143bf9ca89cb9e5f4a27511f8
gronowski <terence.gronowski@gmail.com>
2013-11-13 09:33:44 +0100

    Synchronisation mit Klassenvariable

--- MeinThread.java
+++ MeinThread.java
@@ -5,22 +5,28 @@
  * Time: 20:40
  * To change this template use File | Settings | File Templates.
  */
+
+
 public class MeinThread implements Runnable{
-    public synchronized void run(){
+    static final Object schluessel=new Object();
+
+    public  void run(){
+        synchronized (schluessel){
         for (int i=0;i<10;i++){
          try{
              Thread.sleep(1000);
          }catch(InterruptedException e)  {}
             System.out.println(Thread.currentThread().getName()+" "+ i);
       }
+        }
     }
 
     public static void main(String[] args){
         Thread t0=new Thread(new MeinThread());
-        t0.setName("T1");
+        t0.setName("T0");
         t0.start();
         Thread t1=new Thread(new MeinThread());
-        t1.setName("T2");
+        t1.setName("T1");
         t1.start();
     }
 }
